6e7009eebeef0da99f4a736548bec49f5648b6a8
amos-kibet <kibetamos97@gmail.com>
2022-07-10 11:10:47 +0300

    document atom definition, with code examples

---
+++ atoms.ex
@@ -0,0 +1,17 @@
+defmodule Atomic do
+  @moduledoc """
+  Atoms are constans that represent something's name.
+  An atom's name is its value.
+
+  Examples:
+  :fred, :is_binary?, :var@2, :<>, :===, :"func/3"
+  :"long john silver", :true, :false, :etc
+  """
+
+  def atomic do
+    answer = IO.gets("Enter an atom: ") # any string preceeded by a colon (:)
+    IO.puts("Answer is: #{answer}") # value is the same atom
+  end
+end
+
+Atomic.atomic()
