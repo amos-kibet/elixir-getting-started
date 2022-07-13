3f231f4ef59befea3e717822107bdd074123e7e0
amos-kibet <kibetamos97@gmail.com>
2022-07-13 11:19:13 +0300

    modify factorial.ex
    includes another solution; uncomment to run run

--- factorial.ex
+++ factorial.ex
@@ -1,10 +1,17 @@
-defmodule Factorial do
-  def fact(n) do
-    (n==1 or n==0) && 1 || n * fact(n-1)
-  end
+# defmodule Factorial do
+#   def fact(n) do
+#     (n==1 or n==0) && 1 || n * fact(n-1)
+#   end
 
-end
+# end
 
-{number, _} = IO.gets("Enter a number: ") |> Integer.parse
-answer = Factorial.fact number
-IO.puts("Answer is: #{answer}")
+# {number, _} = IO.gets("Enter a number: ") |> Integer.parse
+# answer = Factorial.fact number
+# IO.puts("Answer is: #{answer}")
+
+# Another solution, using pattern matching
+
+# defmodule Factorial do
+#   def of(0), do: 1
+#   def of(n), do: n * of(n-1)
+# end
